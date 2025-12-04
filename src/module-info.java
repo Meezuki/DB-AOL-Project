a5c7a0f070a107c46be6133b0a9de102ac0f4bc6
Marco <marcoodenleo25@gmail.com>
2025-12-04 13:36:35 +0700

    final

--- src/module-info.java
+++ src/module-info.java
@@ -2,19 +2,13 @@ module Database_Project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql; // Biar bisa connect database
-    requires javafx.base; // Tambahkan ini juga jika belum ada
+    requires javafx.base; 
     requires javafx.graphics;
     
-    // --- TAMBAHKAN BAGIAN INI ---
-    
+
     // Memberi izin JavaFX untuk membaca class di dalam package 'model'
     opens model to javafx.base;
     
-    // Memberi izin JavaFX untuk menjalankan aplikasi utama
-    // (Sesuaikan 'application' dengan nama package tempat MainApp/DashboardWindow Anda berada)
     opens application to javafx.graphics, javafx.fxml;
-    
-    // Jika Anda memakai package 'com.gudang.model' sesuai tutorial awal saya, 
-    // tulisnya: opens com.gudang.model to javafx.base;
-    // Tapi melihat error log Anda, sepertinya package Anda bernama 'model'.
+   
 }
